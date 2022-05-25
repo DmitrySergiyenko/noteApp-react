ed3159112b05bb4d7661bf13c4aa77afe24a156c
Dmitry Sergiyenko <dsergiyenko@astecindustries.com>
2022-05-25 10:13:38 -0500

    manual note array fot test

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+
+export const Form = () => {
+
+    return (
+        <form>
+            <div className="form-group">
+                <input type="text" className="form-control" placeholder="input a note"/>
+            </div>
+        </form>
+    )
+}
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+export const About = () => (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-4">My First React App</h1>
+        <p className="lead">
+          Version of app <strong>1.0.0</strong>
+        </p>
+      </div>
+    </div>
+  )

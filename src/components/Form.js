ee9bb47e6f85be096868da89ef1196d3bc119b5f
Dmitry Sergiyenko <dsergiyenko@astecindustries.com>
2022-05-25 16:27:57 -0500

    Add and fetch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,21 @@
 import { useContext, useState } from "react"
 import { AlertContext } from "../context/alert/alertContext"
+import { FirebaseContext } from "../context/firebase/firebaseContext"
 
 export const Form = () => {
     const [value, setValue] = useState('')
     const alert = useContext(AlertContext)
+    const firebase = useContext(FirebaseContext)
 
     const submitHandler = event => {
         event.preventDefault()
 
         if (value.trim()) {
-
-            alert.show("Note has been created", 'success')
+            firebase.addNote(value.trim()).then(() => {
+                alert.show("Note has been created", 'success')
+            }).catch (() => {
+                alert.show('Something wrong', 'danger')
+            })
             setValue('')
         } else {
             alert.show("Enter a note")
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,27 @@
-import { Fragment } from "react"
+import { Fragment, useContext, useEffect } from "react"
 import { Form } from "../components/Form"
+import { Loader } from "../components/Loader"
 import { Notes } from "../components/Notes"
+import { FirebaseContext } from "../context/firebase/firebaseContext"
 
 
 export const Home = () => {
-    const notes = new Array (3).fill('').map((_, i) => ({id: i, title: `Note ${i+1}`}))
+    // const notes = new Array (3).fill('').map((_, i) => ({id: i, title: `Note ${i+1}`}))
+    const { loading, notes, fetchNotes } = useContext(FirebaseContext)
+
+    useEffect(() => {fetchNotes()}, [])
 
     return (
         <Fragment>
             <Form />
-            <hr/>
+            <hr />
 
-            <Notes notes={notes}/>
+            {loading 
+            ? <Loader/>
+            : <Notes notes={notes} />
+            }
+
+            
 
         </Fragment>
     )
